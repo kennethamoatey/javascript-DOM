1cc3d49f932dc5e07c2cba2aaf7d37583afe2f3c
kenneth amoatey <kenneth.amoatey@gmail.com>
2024-06-06 08:42:34 +0000

    deletion

--- dom1.js
+++ dom1.js
@@ -1,39 +1,30 @@
-/*let img =document.querySelector('img')
-let press=document.querySelector('.btn')
-press.addEventListener('click'()=>{
+const img =document.querySelector("img")
+let btn=document.querySelector(".btn")
+btn.addEventListener('click',()=>{
     if(img.classList.contains('show')){
         img.classList.remove('show')
         img.classList.add('hide')
-        press.textcontent='show'
+         btn.textcontent='show'
 
     } else{
         img.classList.add('show')
         img.classList.remove('hide')
-        press.textcontent='hide'
+        btn.textcontent='hide'
     }
 })
 
-class Square{
-    constructor(height,width){
-        this.height=height;
-        this.width=width;
-    }
-}
-const User1=new Square(20,15)
-console.log(User1)
 
-const User2=new Square(30,13)
-console.log(User2)*/
-
-class Profile{
-    constructor(name,age,gender){
-        this.name=name
-        this.age=age
-        this.gender=gender
-    }
-    getName(){
-        return this.name +" "+ this.age
-    }
+let userName = "kenneth";
+if (userName){
+    console.log(`Hello ${userName}`)
+} else{
+    console.log('Hello!')
 }
-User1=new Profile('Kenneth Amoatey',28,'male')
-console.log(User1.getName())
+let userQuestion='How are you doing today'
+console.log(userQuestion)
+let randomNumber=Math.floor(Math.random()*3);
+console.log(randomNumber);
+let eightBall= '';
+
+
+
